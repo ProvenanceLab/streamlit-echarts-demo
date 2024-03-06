60d576c864c021310b21ba229c90575e25338593
ProvenanceLab <102158709+ProvenanceLab@users.noreply.github.com>
2024-03-06 23:34:48 +0200

    Update bar.py

--- demo_echarts/bar.py
+++ demo_echarts/bar.py
@@ -9,7 +9,7 @@ def render_basic_bar():
             "data": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
         },
         "yAxis": {"type": "value"},
-        "series": [{"data": [120, 200, 150, 80, 70, 110, 130], "type": "bar"}],
+        "series": [{"data": [120, 300, 150, 80, 70, 110, 130], "type": "bar"}],
     }
     st_echarts(options=options, height="500px")
 
